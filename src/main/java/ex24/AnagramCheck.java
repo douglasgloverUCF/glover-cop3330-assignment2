cccd07ba7d25dfc3adc4e28d80f012eb40a35740
Douglas Glover <douglasglover3@knights.ucf.edu>
2021-06-07 19:57:51 -0400

    Simplified solution 24

--- src/main/java/ex24/AnagramCheck.java
+++ src/main/java/ex24/AnagramCheck.java
@@ -1,37 +1,17 @@
 package ex24;
 
 import java.lang.reflect.Array;
+import java.util.Arrays;
 
 public class AnagramCheck {
-    boolean isAnagram(String str1, String str2)
+    boolean isAnagram(String string1, String string2)
     {
-        int i, j;
-        int length1 = str1.length();
-        int length2 = str2.length();
-        boolean[] usedChars = new boolean[length2];
-        boolean valid = false;
+        char[] string1array = string1.toCharArray();
+        char[] string2array = string2.toCharArray();
 
-        if(length1 != length2) return false;
+        Arrays.sort(string1array);
+        Arrays.sort(string2array);
 
-        for (i = 0; i < length1; i++)
-        {
-            for (j = 0; j < length2; j++)
-            {
-                //prevents identical characters from being checked twice
-                if (usedChars[j]) continue;
-
-                if(str1.charAt(i) == str2.charAt(j))
-                {
-                    usedChars[j] = true;
-                    valid = true;
-                    break;
-                }
-            }
-            if (valid)
-                valid = false;
-            else
-                return false;
-        }
-        return true;
+        return Arrays.equals(string1array, string2array);
     }
 }
