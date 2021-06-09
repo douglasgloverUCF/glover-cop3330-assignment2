014a9f875b7be13fbeeda8f2cfd97fe5842dd4fa
Douglas Glover <douglasglover3@knights.ucf.edu>
2021-06-09 16:42:56 -0400

    Solution 29

--- src/test/java/ex29/Sol29Test.java
+++ src/test/java/ex29/Sol29Test.java
@@ -10,24 +10,24 @@ class Sol29Test {
     void returns_0_if_invalid() {
         String input = "ABC";
         double result = app.validateInput(input);
-        assertEquals(result, 0);
+        assertEquals(0, result);
     }
     @Test
     void returns_0_if_invalid_with_numbers() {
         String input = "ABC123";
         double result = app.validateInput(input);
-        assertEquals(result, 0);
+        assertEquals(0, result);
     }
     @Test
     void returns_0_if_0() {
         String input = "0";
         double result = app.validateInput(input);
-        assertEquals(result, 0);
+        assertEquals(0, result);
     }
     @Test
     void returns_value_if_valid() {
         String input = "4";
         double result = app.validateInput(input);
-        assertEquals(result, 4);
+        assertEquals(4, result);
     }
 }
